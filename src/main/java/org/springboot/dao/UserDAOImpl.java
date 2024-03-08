b711807754d16327a6eeeb33fdb98a4ebca71cd3
Lyksan-ruthevul <154101744+Lyksan-ruthevul@users.noreply.github.com>
2024-03-08 03:07:36 +0300

    Задача 3.1.1 Начинаем работу со Spring Boot.

---
+++ src/main/java/org/springboot/dao/UserDAOImpl.java
@@ -0,0 +1,44 @@
+package org.springboot.dao;
+
+import org.springboot.model.User;
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> displayAllUsers() {
+        return entityManager.createQuery("FROM User", User.class).getResultList();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        User updateUser = entityManager.merge(user);
+        entityManager.persist(updateUser);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        User deleteUser = entityManager.merge(user);
+        entityManager.remove(deleteUser);
+    }
+
+}
